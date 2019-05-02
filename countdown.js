fc642362c41ea3a215802571bd070690e15e9f76
Shade Wong <shadeying@gamil.com>
2019-05-02 21:47:51 +0000

    Countdown generator completed

---
+++ countdown.js
@@ -0,0 +1,24 @@
+var countdownGenerator = function (x) {
+
+  return function(){
+    if(x > 0){
+      console.log("T-minus " + x + "...");
+    }
+    if(x === 0){
+      console.log("Blast Off!");
+    }
+    if(x < 0){
+      console.log("Rockets already gone, bub!");
+    }
+
+    x -= 1;
+  }
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
